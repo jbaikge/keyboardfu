9f8c7c201f6da8c8438c7250df107a45331d9647
jake <bigjake2s@gmail.com>
2011-12-19 21:09:37 -0500

    Adding webroot

---
+++ webroot.conf.php
@@ -0,0 +1,5 @@
+<?php
+$config['secret'] = 'd57c6fc8086676a5ca088744888964b592adc50b881b085d7a187aa9c31f9027';
+$config['title'] = 'KeyboardFu';
+
+require(dirname(__FILE__) . '/lib/framework/load.php');
