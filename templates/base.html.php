0c69601f1d66007e571310a8494bcfe4d06247a1
jake <bigjake2s@gmail.com>
2011-12-19 21:19:43 -0500

    Adding the base of the base template

---
+++ templates/base.html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title><?php echo Meta::getTitle(); ?></title>
+	<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<link rel="stylesheet" type="text/css" href="/css/html5.css">
+	<![endif]-->
+	<link rel="profile" href="http://microformats.org/profile/hcard">
+</head>
+<body>
+</body>
+</html>
