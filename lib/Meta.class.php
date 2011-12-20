0f58f0a3b12d0292d6be8b137f027f2e6cad7dc2
jake <bigjake2s@gmail.com>
2011-12-19 20:46:53 -0500

    Imported the Meta class from the 300Brand website.

--- lib/Meta.class.php
+++ lib/Meta.class.php
@@ -1,16 +1,29 @@
 <?php
 class Meta {
+	private static $javascripts = array();
 	private static $stylesheets = array();
-	private static $title = null;
-
-	public static function addStylesheet($path) {
-		self::$stylesheets[] = $path;
+	public static function addJavascript($url) {
+		self::$javascripts[$url] = sprintf('<script src="%s"></script>', $url);
 	}
-
+	public static function addStylesheet($url) {
+		self::$stylesheets[$url] = sprintf('<link rel="stylesheet" type="text/css" href="%s">', $url);
+	}
+	public static function getAll() {
+		return array_merge(self::$stylesheets, self::$javascripts);
+	}
+	public static function getJavascripts() {
+		return self::$javascripts;
+	}
 	public static function getStylesheets() {
 		return self::$stylesheets;
 	}
-
+	public static function toString($prefix = "\t") {
+		$string = '';
+		foreach (Meta::getAll() as $tag) {
+			$string .= sprintf("%s%s\n", $prefix, $tag);
+		}
+		return $string;
+	}
 	public static function getTitle() {
 		$title = $_ENV['config']['title'];
 		if (self::$title) {
