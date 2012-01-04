4e32e96df0ee402e5b7cc225a0fb7f4edcfe9a3b
jake <bigjake2s@gmail.com>
2012-01-04 12:30:06 -0500

    Nailed down the JSONStorage driver. Pushed the filename for the JSON file on the implementing object.

--- lib/JSONStorage.interface.php
+++ lib/JSONStorage.interface.php
@@ -1,9 +1,12 @@
 <?php
 interface JSONStorage {
-	public function getJSONFilename();
+	public function getJSONFilename($uuid = null);
 }
 
-class JSONStorageDriver extends FObjectDriver implements FObjectPopulateHooks, FObjectUpdateHooks {
+class JSONStorageDriver extends FObjectDriver implements /*FObjectPopulateHooks,*/ FObjectUpdateHooks {
+	private $filename;
+	private $jsonData;
+	/*
 	public function prePopulate(&$data) {
 		if (!is_array($data)) {
 			$data = array();
@@ -11,5 +14,26 @@ class JSONStorageDriver extends FObjectDriver implements FObjectPopulateHooks, F
 	}
 	public function doPopulate(&$data) {
 
+	}
+	*/
+	public function preUpdate(&$data) {
+		$this->filename = $this->subject->getJSONFilename();
+		$this->ensureDir(dirname($this->filename));
+	}
+	public function doUpdate(&$data) {
+		$this->jsonData = json_encode($this->subject->getData());
+	}
+	public function postUpdate(&$data) {
+		file_put_contents($this->filename, $this->jsonData);
+		$this->jsonData = null;
+	}
+	public function failUpdate($exception) {
+		$this->jsonData = null;
+	}
+	private function ensureDir($dir) {
+		if (!is_dir($dir)) {
+			mkdir($dir);
+			@chmod($dir, 0777);
+		}
 	}
 }
