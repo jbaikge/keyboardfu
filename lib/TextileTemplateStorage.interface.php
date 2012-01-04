205d298fdc2a1d896e7c179a4861acf88559cf3b
jake <bigjake2s@gmail.com>
2012-01-04 12:31:05 -0500

    Simplified the required functions for the TextileTemplateStorage interface. Added a function for adding new textile files to allow multiples.

--- lib/TextileTemplateStorage.interface.php
+++ lib/TextileTemplateStorage.interface.php
@@ -1,36 +1,49 @@
 <?php
 interface TextileTemplateStorage {
-	public function getTextileFiles();
-	public function getTextileTemplateDir();
+	public function getTextileCompiledFilename($uuid = null);
 }
 
 class TextileTemplateStorageDriver extends FObjectDriver implements FObjectUpdateHooks {
 	private $compiled = array();
 	private $textile;
+	private $textileFiles = array();
+
+	public function addTextileFile($filename) {
+		if (!file_exists($filename) || !is_file($filename)) {
+			throw new InvalidTextileFileException('Invalid Textile file: ' . $filename);
+		}
+		$page_num = count($this->textileFiles) + 1;
+		$compiled = $this->getCompiledFilename($page_num);
+		$this->textileFiles[$filename] = $compiled;
+		$this->pages[] = $compiled;
+	}
 
 	public function preUpdate(&$data) {
 		$this->textile = new Textile();
-		if (!is_dir($this->getTextileTemplateDir())) {
-			mkdir($this->getTextileTemplateDir());
+		$dir = dirname($this->subject->getTextileCompiledFilename());
+		if (!is_dir($dir)) {
+			mkdir($dir);
+			@chmod($dir, 0777);
 		}
 	}
 	public function doUpdate(&$data) {
-		foreach ($this->subject->getTextileFiles() as $i => $file) {
+		foreach ($this->textileFiles as $file => $save_as) {
 			$raw = file_get_contents($file);
-			$this->compiled[] = $textile->TextileThis($raw);
+			$this->compiled[$save_as] = $this->textile->TextileThis($raw);
 		}
 	}
 	public function postUpdate(&$data) {
-		foreach ($this->compiled as $i => $content) {
-			$filename = sprintf('%s/%s-%02d.html.php',
-				$this->getTextileTemplateDir(),
-				$this->subject->uuid,
-				$i + 1
-			);
+		foreach ($this->compiled as $filename => $content) {
 			file_put_contents($filename, $content);
 		}
 	}
-	public function failUpdate(&$data) {
+	public function failUpdate($exception) {
 		// nothing needed
 	}
+
+	private function getCompiledFilename($page) {
+		return sprintf($this->subject->getTextileCompiledFilename(), $page);
+	}
 }
+
+class InvalidTextileFileException extends Exception {}
