9a56673000035bb5d967d6d0a2aa6b1a1ec32f14
jake <bigjake2s@gmail.com>
2012-01-05 21:18:45 -0500

    Built out the article display mechanism so it actually works and works quite well.

--- article.php
+++ article.php
@@ -5,22 +5,23 @@ require('webroot.conf.php');
 $path = filter_input(INPUT_GET, 'path', FILTER_SANITIZE_STRING);
 if ($path == '') {
 	header('HTTP 1.1 404 Not Found');
+	echo 'no path supplied';
 	exit;
 }
 
-$article = Article::getFromURL($path);
+$article = ArticleMap::getArticle($path);
 if ($article == null) {
 	header('HTTP 1.1 404 Not Found');
+	echo 'article not found';
 	exit;
 }
 
 // Push in meta information
 Meta::setTitle($article->title);
-foreach ($article->getStylesheets() as $stylesheet) {
-	Meta::addStylesheet($stylesheet);
-}
-
-$body = FTemplate::fetch($article->getTextileTemplate());
+//foreach ($article->getStylesheets() as $stylesheet) {
+//	Meta::addStylesheet($stylesheet);
+//}
+$body = FTemplate::fetch($article->pages[0]);
 
 // Render
 $content = FTemplate::fetch('templates/article.html.php');
