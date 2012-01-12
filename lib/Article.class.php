048d60d1a95fdfb92ec606a7bcb0cd530686a0c8
jake <bigjake2s@gmail.com>
2012-01-11 21:01:54 -0500

    Finalized some functionality for getting rudimentary components of the Article out.

--- lib/Article.class.php
+++ lib/Article.class.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @author Jake Tews <jake@keyboardfu.com>
+ * @date Tue Dec 13 19:00:35 2011 -0500
+ */
 class Article extends FObject {
 	public function __construct($name) {
 		if (!file_exists($this->getFilename($name))) {
@@ -8,20 +12,46 @@ class Article extends FObject {
 		}
 	}
 	public function getFilename($name) {
-		return $_ENV['config']['cache.dir'] . '/articles/' . $name . '/data.php';
+		return $this->getStorageDir($name) . '/data.php';
 	}
 	public static function getFromURL($url) {
 		return new Article(basename($url));
 	}
 	public static function getModel() {
-		return new FModelManager(array(
+		return new FModelManager(
 			FField::make('modified'),
+			FField::make('name'),
 			FField::make('pages'),
 			FField::make('published'),
+			FField::make('stylesheets'),
 			FField::make('tags'),
 			FField::make('title'),
-			FField::make('url'),
-		));
+			FField::make('url')
+		);
+	}
+	/*!
+	 * Fetches the appropriate template for the specified page within an
+	 * Article. If a suitable page is not found, the first page is returned.
+	 * 
+	 * @param $num Page number
+	 * @return Filepath to the HTML template.
+	 */
+	public function getPage($num = 1) {
+		$dir = $this->getStorageDir();
+		$filename = sprintf("%s/%02d.html.php", $dir, $num);
+		if ($num != 1 && !file_exists($filename)) {
+			return $this->getPage();
+		}
+		return $filename;
+	}
+	public function getStorageDir($name = null) {
+		if ($name === null) {
+			$name = $this->name;
+		}
+		return $_ENV['config']['cache.dir'] . '/articles/' . $name;
+	}
+	public function getStylesheets() {
+		return is_array($this->stylesheets) ? $this->stylesheets : array();
 	}
 }
 
