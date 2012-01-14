6df0c966e4c7e08e974d06810ed4d3154d732aaa
jake <bigjake2s@gmail.com>
2012-01-13 22:49:40 -0500

    Added redirect fall-down processing of the archive area to incrementally push users back to broader scopes until one is found or they are forwarded to the homepage.

--- archive.php
+++ archive.php
@@ -2,19 +2,36 @@
 require('webroot.conf.php');
 $date = filter_input(INPUT_GET, 'date', FILTER_SANITIZE_STRING);
 
-$date_bits = explode('/', trim($date, '/'));
+$bits = explode('/', trim($date, '/'));
 
-switch (count($date_bits)) {
+switch (count($bits)) {
 	// Daily archive
 	case 3:
-	break;
+		$articles = Archive::getInstance()->getDaily($bits[0], $bits[1], $bits[2]);
+		if (empty($articles)) {
+			header('Location: ' . WEBROOT . '/' . $bits[0] . '/' . $bits[1]);
+			exit;
+		}
+		break;
 	// Monthly archive
 	case 2:
-	break;
+		$articles = Archive::getInstance()->getMonthly($bits[0], $bits[1]);
+		if (empty($articles)) {
+			header('Location: ' . WEBROOT . '/' . $bits[0]);
+			exit;
+		}
+		break;
 	// Yearly archive
 	case 1:
-		var_dump(Archive::getInstance()->getYearly($date_bits[0]));
-	break;
+		$articles = Archive::getInstance()->getYearly($bits[0]);
+		if (!empty($articles)) {
+			break;
+		}
+	// No date supplied (direct link to archive.php) or empty yearly archive.
+	default:
+		header('Location: ' . WEBROOT . '/');
+		exit;
 }
+
 
 FTemplate::render();
