4fb437cda8243eb7965d199a67d47c1080ef9886
jake <bigjake2s@gmail.com>
2012-01-16 10:32:17 -0500

    Built out the archive section.

--- archive.php
+++ archive.php
@@ -5,25 +5,30 @@ $date = filter_input(INPUT_GET, 'date', FILTER_SANITIZE_STRING);
 $bits = explode('/', trim($date, '/'));
 
 switch (count($bits)) {
-	// Daily archive
+	// Daily archive. If none exist, redirect to the monthly archive page.
 	case 3:
 		$articles = Archive::getInstance()->getDaily($bits[0], $bits[1], $bits[2]);
 		if (empty($articles)) {
 			header('Location: ' . WEBROOT . '/' . $bits[0] . '/' . $bits[1]);
 			exit;
 		}
+		$timestamp = strtotime(implode('-', $bits));
+		Meta::setTitle('Archive for ' . FString::date($timestamp));
 		break;
-	// Monthly archive
+	// Monthly archive. If none exist, redirect to the yearly archive page.
 	case 2:
 		$articles = Archive::getInstance()->getMonthly($bits[0], $bits[1]);
 		if (empty($articles)) {
 			header('Location: ' . WEBROOT . '/' . $bits[0]);
 			exit;
 		}
+		$timestamp = strtotime(implode('-', $bits) . '-01');
+		Meta::setTitle('Archive for ' . FString::date($timestamp, 'M, Y'));
 		break;
-	// Yearly archive
+	// Yearly archive. If none exist, fall through to the default action.
 	case 1:
 		$articles = Archive::getInstance()->getYearly($bits[0]);
+		Meta::setTitle('Archive for ' . $bits[0]);
 		if (!empty($articles)) {
 			break;
 		}
@@ -33,5 +38,10 @@ switch (count($bits)) {
 		exit;
 }
 
+$list = array_reduce($articles, function($html, $article) {
+	return $html . FTemplate::fetchCached('templates/archive-item.html.php', array('article' => &$article));
+}, '');
+
+$content = FTemplate::fetch('templates/archive.html.php');
 
 FTemplate::render();
---
+++ templates/archive-item.html.php
@@ -0,0 +1,8 @@
+<article>
+	<h2><?php e($article->title); ?></h2>
+	<?php echo $article->abstract; ?>
+	<footer>
+		<time>Published: <?php echo FString::date($article->published); ?></time>
+		<div class="readMore"><a href="<?php echo $article->url; ?>">Read More</a></div>
+	</footer>
+</article>
