6a7016dc78eb4a4ea5ce3b83236d69e009da46df
jake <bigjake2s@gmail.com>
2012-01-24 19:01:24 -0500

    Implementing the article footer the way I'd like it to look.

--- templates/article_footer.html.php
+++ templates/article_footer.html.php
@@ -7,27 +7,7 @@ if ($article->pages == 1) {
 <footer>
 	<nav>
 		<ol>
-<?php
-if ($page == 1) {
-	echo "\t\t\t<li>&laquo; Prev</li>\n";
-} else {
-	printf("\t\t\t<li><a href=\"/%s/%d\">&laquo; Prev</a></li>\n", $article->url, $page - 1);
-}
-
-foreach (range(1, $article->pages) as $num) {
-	if ($num == $page) {
-		printf("\t\t\t<li>%d</li>\n", $num);
-	} else {
-		printf("\t\t\t<li><a href=\"/%s/%d\">%d</a></li>\n", $article->url, $num, $num);
-	}
-}
-
-if ($page == $article->pages) {
-	echo "\t\t\t<li>Next &raquo;</li>\n";
-} else {
-	printf("\t\t\t<li><a href=\"/%s/%d\">Next &raquo;</a></li>\n", $article->url, $page + 1);
-}
-?>
+<?php echo $paging_links; ?>
 		</ol>
 	</nav>
 </footer>
