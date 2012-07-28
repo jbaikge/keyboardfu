ddd23b8f48057eae79fee3438670c9fc1eccbe14
jake <bigjake2s@gmail.com>
2012-07-28 00:01:57 -0400

    Brought in Archive::getLastN()

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require('webroot.conf.php');
 
-$latest = Archive::getInstance()->getLatest();
+$article_list = '';
+foreach (Archive::getInstance()->getLastN(5) as $article) {
+	$article_list .= $article->title;
+}
 $content = FTemplate::fetch('templates/index.html.php');
 FTemplate::render();
