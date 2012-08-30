f0d32b016766f31aebd4936976680d872262498c
jake <bigjake2s@gmail.com>
2012-08-29 20:28:58 -0400

    Adding basis for resume

---
+++ resume.php
@@ -0,0 +1,3 @@
+<?php
+require('webroot.conf.php');
+FTemplate::render('templates/resume.html.php');
---
+++ templates/resume.html.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jacob Tews - Resume</title>
+<style type="text/css">
+@media screen {
+	/* Turn page into paper */
+	html {
+		background:#CCC;
+	}
+	body {
+		background:#FFF;
+		box-shadow:0 0 0.5em 0 #333;
+		height:10in;
+		margin:1em auto;
+		padding:1in;
+		width:7.5in;
+	}
+}
+</style>
+</head>
+<body>
+<header>
+	<h1>Jacob Tews</h1>
+</header>
+<section id="Objective">
+	<h1>Objective</h1>
+	<article>
+		<p>Continue along the path of programming Zen through exploratory projects utilizing: Go, PHP, data management, distributed computing, web frontends, Linux backends, and massive amounts of brain power.</p>
+	</article>
+</section>
+</body>
+</html>
