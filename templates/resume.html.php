62ec76bb0e97736dc56246de6eb9f1dc3e858697
jake <bigjake2s@gmail.com>
2012-08-29 20:47:59 -0400

    Work experience list

--- templates/resume.html.php
+++ templates/resume.html.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Jacob Tews - Resume</title>
-<style type="text/css">
-@media screen {
-	/* Turn page into paper */
-	html {
-		background:#CCC;
-	}
-	body {
-		background:#FFF;
-		box-shadow:0 0 0.5em 0 #333;
-		height:10in;
-		margin:1em auto;
-		padding:1in;
-		width:7.5in;
-	}
-}
-</style>
+	<title>Jacob Tews - Resume</title>
+	<meta charset="UTF-8">
+	<style type="text/css">
+		@media screen {
+			/* Turn page into paper */
+			html {
+				background:#222;
+			}
+			body {
+				background:#444;
+				box-shadow:0 0 0.5em 0 #333;
+				color:#CCC;
+				height:10in;
+				margin:1em auto;
+				padding:1in;
+				width:7.5in;
+			}
+		}
+	</style>
 </head>
 <body>
+<!--
+Before anyone says anything about multiple h1s in a document, please run this
+thing through w3's validator.
+-->
 <header>
 	<h1>Jacob Tews</h1>
 </header>
@@ -27,6 +33,47 @@
 	<h1>Objective</h1>
 	<article>
 		<p>Continue along the path of programming Zen through exploratory projects utilizing: Go, PHP, data management, distributed computing, web frontends, Linux backends, and massive amounts of brain power.</p>
+	</article>
+</section>
+<section id="Contact">
+	<h1>Contact Information</h1>
+	<dl>
+		<dt>Phone</dt>
+		<dd>571.572.9577</dd>
+		<dt>Email</dt>
+		<dd>jake@keyboardfu.com</dd>
+		<dt>Address</dt>
+		<dd>Available upon request</dd>
+	</dl>
+</section>
+<section id="Experience">
+	<h1>Experience</h1>
+	<article>
+		<header>
+			<h2>300Brand</h2>
+			<div class="dates">May, 2009 - Present</div>
+			<div class="url"><a href="http://300brand.com" target="_blank">http://300brand.com</a></div>
+			<div class="location">Alexandria, VA</div>
+			<div class="position">Lead Developer, Database Administrator, System Administrator</div>
+		</header>
+	</article>
+	<article>
+		<header>
+			<h2>Web Teks, Inc.</h2>
+			<div class="dates">March, 2006 - May, 2009</div>
+			<div class="url"><a href="http://www.webteks.com" target="_blank">http://www.webteks.com</a></div>
+			<div class="location">Chesapeake, VA</div>
+			<div class="position">Lead Developer, Database Administrator, System Administrator</div>
+		</header>
+	</article>
+	<article>
+		<header>
+			<h2>Virginia Beach City Public Schools</h2>
+			<div class="dates">October, 2004 - March, 2006</div>
+			<div class="url"><a href="http://www.vbschools.com" target="_blank">http://www.vbschools.com</a></div>
+			<div class="location">Virginia Beach, VA</div>
+			<div class="position">Assistant Webmaster</div>
+		</header>
 	</article>
 </section>
 </body>
