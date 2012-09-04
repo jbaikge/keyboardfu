8e31f9f04d30d996d3b274c052f933cd93c50566
jake <bigjake2s@gmail.com>
2012-09-04 12:40:06 -0400

    Starting TODO list

--- playground/index.php
+++ playground/index.php
@@ -4,3 +4,7 @@ require('webroot.conf.php');
 Meta::setTitle('Playground');
 
 FTemplate::render();
+
+# 300brand.com - JS Slider, autostop on click
+# 300brand.com/who - JS transitions, follows same theme
+# RSS Summary - Markov article bodies and regurgitate a paragraph summary
