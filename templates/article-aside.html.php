e44dccf0030ca0c2d9d794b2ff77746598a3e5e6
jake <bigjake2s@gmail.com>
2012-09-11 20:43:13 -0400

    Overall layout tweaks. Pretty pleased finally

--- templates/article-aside.html.php
+++ templates/article-aside.html.php
@@ -4,24 +4,25 @@
 			<dl>
 <?php foreach (Archive::getInstance()->getLastN($_ENV['config']['latest']) as $a) { ?>
 				<dt><a href="<?php e($a->url); ?>"><?php e($a->title); ?></a></dt>
-				<dd><?php echo FString::date($a->published); ?></dd>
+				<dd><?php echo FString::date($a->published, 'M d'); ?></dd>
 <?php } ?>
 			</dl>
 		</section>
 		<section class="archives">
 			<h1>Archives</h1>
-			<ol>
+			<dl>
 <?php foreach (Archive::getInstance()->getCalendarTree() as $month => $count) { ?>
-				<li><a href="<?php echo FString::date($month, "Y/m"); ?>"><?php echo FString::date($month, 'M Y'); ?></a><sup><?php echo $count; ?></sup></li>
+				<dt><a href="<?php echo FString::date($month, "Y/m"); ?>"><?php echo FString::date($month, 'M Y'); ?></a></dt>
+				<dd><?php echo $count; ?></dd>
 <?php } ?>
-			</ol>
+			</dl>
 		</section>
 		<section class="tags">
 			<h1>Tags</h1>
-			<ol>
+			<dl>
 <?php foreach (Tags::getInstance() as $tag) { ?>
-				<li><a href="/tag/<?php echo rawurlencode($tag); ?>"><?php e($tag); ?></a></li>
+				<dt><a href="/tag/<?php echo rawurlencode($tag); ?>"><?php e($tag); ?></a></dt>
 <?php } ?>
-			</ol>
+			</dl>
 		</section>
 	</aside>
