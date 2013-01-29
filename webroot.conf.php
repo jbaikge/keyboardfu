c6700f6c2799d01e38abfa6079f252d808083dc7
jake <bigjake2s@gmail.com>
2013-01-28 20:55:42 -0500

    Support for Bootstrap auto-loading in webroot.

--- webroot.conf.php
+++ webroot.conf.php
@@ -6,5 +6,10 @@ $config['mode.production'] = strpos(__FILE__, '/home/jbaikge') === 0;
 
 require(dirname(__FILE__) . '/lib/framework/load.php');
 
-Meta::addStylesheet('/css/base.css');
-Meta::addStylesheet('//fonts.googleapis.com/css?family=Droid+Sans+Mono|Droid+Serif|Droid+Sans');
+if (defined('USE_BOOTSTRAP')) {
+	Meta::addStylesheet('/css/bootstrap.min.css');
+	Meta::addJavascript('/js/bootstrap.min.js');
+} else {
+	Meta::addStylesheet('/css/base.css');
+	Meta::addStylesheet('//fonts.googleapis.com/css?family=Droid+Sans+Mono|Droid+Serif|Droid+Sans');
+}
